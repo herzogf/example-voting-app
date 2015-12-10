14094e60f719cc9741c3db7111e3df861528807a
herzogf <herzog-florian@gmx.de>
2015-12-10 20:21:00 +0100

    Update app.py

--- vote-apps/voting-app/app.py
+++ vote-apps/voting-app/app.py
@@ -8,8 +8,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Wuestenrot")
-option_b = os.getenv('OPTION_B', "Schwaebisch Hall")
+option_a = os.getenv('OPTION_A', "Cats")
+option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
 redis_hostname = os.getenv('REDIS_HOST', "voteapps_redis_1")
